f42ba755aa93f16e597d7b218d6253613c370d35
hcebulla <hannahmcebulla@gmail.com>
2019-12-05 15:08:27 -0700

    Update controlApp.py

--- controlApp.py
+++ controlApp.py
@@ -1,64 +1,94 @@
-<!This document creates the framework for the web page that will control the RC car.
- Author: Hannah Cebulla
- Date: 11/20/2019
- >
+from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
+import time
 
-<html>
+#create flask app server
+app = Flask(__name__)
 
-<!Establishing the connection between the Python funcitons and the HTML buttons>
+#define pins
+in1 = 24
+in2 = 23
+ENA = 25
+temp1 = 1
+servoPIN = 17
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript"> 
-$('#left').click(function(){
-	$.ajax({
-	type: "POST", "GET", 
-	url: "~/web-server/controlApp.py",
-	});
-	});
-$('#right').click(function(){
-	$.ajax({
-	type: "POST", "GET", 
-	url: "~/web-server/controlApp.py",
-	});
-	});
-$('#stop').click(function(){
-	$.ajax({
-	type: "POST", "GET", 
-	url: "~/web-server/controlApp.py",
-	});
-	});
-$('#forward').click(function(){
-	$.ajax({
-	type: "POST", "GET", 
-	url: "~/web-server/controlApp.py",
-	});
-	});
-$('#reverse').click(function(){
-	$.ajax({
-	type: "POST", "GET", 
-	url: "~/web-server/controlApp.py",
-	});
-	});
-$('#center').click(function(){
-	$.ajax({
-	type: "POST", "GET", 
-	url: "~/web-server/controlApp.py",
-	});
-	});	
+#setting up pins to receive information 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(ENA,GPIO.OUT)
+#initial setting low
+GPIO.output(in2,GPIO.LOW)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(servoPIN, GPIO.LOW)
+p = GPIO.PWM(ENA, 1000) #set DC PWM to 1000
+q = GPIO.PWM(servoPIN, 50) #set servo PWM to 50Hz
+GPIO.setwarnings(False)
+#initial PWM signal
+p.start(75)
+q.start(7.5)
 
-</script>
-  <body>
+@app.route('/')
+def index():
+#returns the HTML file template 
+    return render_template('index.html')
 
-		<h1>EGEN 310 CONTROL APP</h1>
+#linking the python functions to the flask buttons
 
-		<!Creating the buttons>
+@app.route('/<action>')
+def left(action):
+    if action == "on":
+        q.ChangeDutyCycle(5)
+        time.sleep(.5)
+        #returns homepage
+        return render_template('index.html')
 
-		<a href="/on" class = "button">FORWARD</a>
-		<a href="/on" class = "button">LEFT</a>
-		<a href="/on" class = "button">CENTER</a>
-		<a href="/on" class = "button">RIGHT</a>
-		<a href="/on" class = "button">REVERSE</a>
-		<a href="/on" class = "button">STOP</a>
-	</body>
-</html>
+   
+@app.route('/<action>')
+def forward(action):
+    if action == "on":
+        GPIO.output(in2, GPIO.HIGH)
+        GPIO.output(in1, GPIO.LOW)
+        #returns homepage
+        return render_template('index.html')
+
+@app.route("/<action>")
+def backward(action):
+    if action == "on":
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in1,GPIO.HIGH)
+        #returns homepage
+        return render_template('index.html')
+
+@app.route("/center")
+def center():
+    if action == "on":
+	q.ChangeDutyCycle(7.5)
+	time.sleep(.5)
+        #returns homepage
+        return render_template('index.html')
+
+@app.route("/right")
+def right():
+    if action == "on":
+	q.ChangeDutyCycle(10)
+	time.sleep(.5)
+        #returns homepage
+        return render_template('index.html')
+	
+
+@app.route('/<action>')
+def stop(action):
+    if action == "on":
+        GPIO.output(in2, GPIO.LOW)
+        GPIO.output(in1, GPIO.LOW)
+        #returns homepage
+        return render_template('index.html')
+
+
+#takes IP address of raspberry pi for web server
+
+if __name__ == '__main__':
+    app.run(debug=True, host='192.168.1.11')
 
